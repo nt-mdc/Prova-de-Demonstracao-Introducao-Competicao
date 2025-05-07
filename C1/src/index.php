a9a624768d0bf4f1e72958b8f3421249d6369dc0
Nathan Mendonça <nthn.mdc@gmail.com>
2025-05-07 16:55:59 -0300

    Save commit

---
+++ C1/src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zip Folder</title>
+</head>
+<body>
+
+    <main>
+        <h3>Zip Folder</h3>
+        <br>
+        <form action="">
+            <input type="file" name="folder" id="folder" webkitdirectory directory multiple>
+        </form>
+    </main>
+
+</body>
+</html>
