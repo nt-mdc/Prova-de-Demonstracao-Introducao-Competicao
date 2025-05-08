c480eda4907cc6b7e1dfc7e11fb4099b502eb21c
Nathan Mendonça <nthn.mdc@gmail.com>
2025-05-08 17:35:48 -0300

    C1 - First Attemp

--- C1/src/index.php
+++ C1/src/index.php
@@ -1,19 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zip Folder</title>
-</head>
-<body>
+<?php
 
-    <main>
-        <h3>Zip Folder</h3>
-        <br>
-        <form action="">
-            <input type="file" name="folder" id="folder" webkitdirectory directory multiple>
-        </form>
-    </main>
+function zipFolder($sourceFolder, $zipFilePath) {
 
-</body>
-</html>
+    if (!extension_loaded('zip') || !file_exists($sourceFolder)){
+        return false;
+    }
+
+    $zip = new ZipArchive();
+
+    if(!$zip->open($zipFilePath, ZipArchive::CREATE | ZipArchive::OVERWRITE)){
+        return false;
+    }
+
+    $sourceFolder = realpath($sourceFolder);
+
+    $files = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($sourceFolder),
+        RecursiveIteratorIterator::LEAVES_ONLY
+    );
+
+    foreach ($files as $name => $file) {
+        if (!$file->isDir()) {
+            $filePath = $file->getRealPath();
+            $relativePath = substr($filePath, strlen($sourceFolder) + 1);
+            $zip->addFile($filePath, $relativePath);
+        }
+    }
+
+    $zip->close();
+
+    return true;
+    
+}
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $pasta = $_POST['pasta'];
+    $arqZip = $pasta.'.zip';
+
+    if(zipFolder($pasta, $arqZip)) {
+        echo "Pasta criada com sucesso! <a href='$arqZip'>Baixar Zip</a>";
+    } else {
+        echo "Falha ao compactar a pasta";
+    }
+}
+?>
+
+<form method="post">
+    <label>Nome da Pasta para Compactar:</label><br>
+    <input type="text" name="pasta" value="minha_pasta" required><br><br>
+    <input type="submit" value="Compactar">
+</form>
